d67a7b0c3bd7a07d5d2249f428b6e49f9b56141d
eddy-qiu <eqiu22@gmail.com>
2023-03-07 02:36:37 -0500

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+user_input = input("Enter a valid propositional statement: ")
+num_variables = input("How many variables are there?")
+
+for j in range(0, user_input.len()):
+        if(user_input[i] == "("):
+
+## remove white space
+## recursively evaulate statements (if there is parenthesis call again
+## do this for every combination of 1 and 0
+
+def negation(variable):
+    return not variable
+
+def conjunction(variable1, variable2):
+    return variable1 and variable2
+
+def disjunction(variable1, variable2):
+    return variable1 or variable2
+
+
+
+
