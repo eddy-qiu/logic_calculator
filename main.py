2d3da57b4f5be1183b404816c06da2412fe1f85e
eddy-qiu <eqiu22@gmail.com>
2023-03-12 00:02:44 -0500

    kinda works but only up to 4 vars - need to add dynamic

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 user_input = input("Enter a valid propositional statement: ")
-num_variables = input("How many variables are there?")
+num_variables = int(input("How many variables are there?"))
+truth_value = 0
 
-for j in range(0, user_input.len()):
-        if(user_input[i] == "("):
+for x1 in range(0, 2):
+    for x2 in range(0, 2):
+        for x3 in range(0, 2):
+            for x4 in range(0, 2):
+                if eval(user_input):
+                    truth_value = 1
+                    break
 
-## remove white space
-## recursively evaulate statements (if there is parenthesis call again
-## do this for every combination of 1 and 0
-
-def negation(variable):
-    return not variable
-
-def conjunction(variable1, variable2):
-    return variable1 and variable2
-
-def disjunction(variable1, variable2):
-    return variable1 or variable2
-
-
-
-
+if truth_value:
+    print("The propositional statement is satisfiable")
+else:
+    print("the propositional statement is unsatisfiable")
