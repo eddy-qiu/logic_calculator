cfdc936aa307b16c9e69ffbd555eba210dd42677
eddy-qiu <eqiu22@gmail.com>
2023-03-13 00:50:25 -0400

    commented

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+# Receive input from user
+# user_input = a valid propositional statement using x1, x2, x3, etc.
+# num_variables = number of xs used (x1, x2 would be 2 vars)
 user_input = input("Enter a valid propositional statement: ")
 num_variables = int(input("How many variables are there?"))
 glb = {}
 truth_value = 0
 
+# Put all the variables into the dictionary
 for i in range(num_variables):
-    glb["x" + str(i+1)] = 0
+    glb["x" + str(i + 1)] = 0
 
+# Loop through all the binary representations of all the possible values of the variables
+# ie 2^(3 variables-1) will produce 111
 for i in range(2 ** num_variables - 1):
+    # evaluate first digit
     while i > 0:
         if i % 2 == 1:
-            glb["x" + str(i+1)] = 1
+            # if the first digit is 1, add the variable = 1 to the dictionary
+            glb["x" + str(i + 1)] = 1
         else:
-            glb["x" + str(i+1)] = 0
+            # else if the first digit is 0, add the variable = 0 to the dictionary
+            glb["x" + str(i + 1)] = 0
+        # dividing by two takes away a digit in binar
         i /= 2
+
+    # bitwise-ly evaluate the user_input given the dictionary values
+    # eval is able to calculate propositions because it uses the same notations as bitwise ( | & ~ () )
     if eval(user_input, glb):
+        # if the evaluation is true, set truth value to true and break the loop
         truth_value = 1
         break
 
+# print the result
 if truth_value:
     print("The propositional statement is satisfiable")
 else:
