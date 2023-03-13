d668a1a13b84653b899e30138abdce9e0a733723
eddy-qiu <eqiu22@gmail.com>
2023-03-13 00:33:47 -0400

    binary representation implemented to create all possible values

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 user_input = input("Enter a valid propositional statement: ")
 num_variables = int(input("How many variables are there?"))
+glb = {}
 truth_value = 0
 
-for x1 in range(0, 2):
-    for x2 in range(0, 2):
-        for x3 in range(0, 2):
-            for x4 in range(0, 2):
-                if eval(user_input):
-                    truth_value = 1
-                    break
+for i in range(num_variables):
+    glb["x" + str(i+1)] = 0
+
+for i in range(2 ** num_variables - 1):
+    while i > 0:
+        if i % 2 == 1:
+            glb["x" + str(i+1)] = 1
+        else:
+            glb["x" + str(i+1)] = 0
+        i /= 2
+    if eval(user_input, glb):
+        truth_value = 1
+        break
 
 if truth_value:
     print("The propositional statement is satisfiable")
